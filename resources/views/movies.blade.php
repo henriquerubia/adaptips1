a6161874e0d4161d0e43645346d49c15f6db816c
Henrique Rubia <henriquerubiab@gmail.com>
2021-09-05 16:02:51 -0300

    Encontro de sexta-feira

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies | Adapti</title>
+</head>
+<body>
+    @foreach ($movies as $movie)
+        <h4>{{ $movie->title }}</h4>
+        <img src="{{ $movie->image }}" alt="Imagem"/>
+    @endforeach
+</body>
+</html>
